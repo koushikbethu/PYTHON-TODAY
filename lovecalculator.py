d55e54a62c847581f4254592c9301b792d4b2383
Koushik <koushikbethu.indian@gmail.com>
2025-10-01 18:53:45 +0530

    Create lovecalculator.py

---
+++ lovecalculator.py
@@ -0,0 +1,26 @@
+name1=input("enter your name in capital:")
+name2=input("enter your :) name:")
+Bothnames=name1+name2
+print(Bothnames)
+small_letters=Bothnames.lower()
+t=small_letters.count('t')
+r=small_letters.count('r')
+u=small_letters.count('u')
+e=small_letters.count('e')
+true=t+r+u+e
+l=small_letters.count('l')
+o=small_letters.count('o')
+v=small_letters.count('v')
+e=small_letters.count('e')
+love=l+o+v+e
+lovescore=int(str(true)+str(love))
+
+if lovescore < 10 or lovescore > 90:
+    print("You are Made for each other")
+elif lovescore<=40 or lovescore>=50:
+    print("You are very lucky to have your partner")
+else:
+    print("You are the Unique couple in the world")
+
+
+
